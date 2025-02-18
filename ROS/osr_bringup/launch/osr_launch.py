6361e325cf75d5694c18138631df208bd25fc185
pine <dijsds@163.com>
2025-02-18 15:46:23 +0800

    fix: Canonical ros node launch file

--- ROS/osr_bringup/launch/osr_launch.py
+++ ROS/osr_bringup/launch/osr_launch.py
@@ -5,10 +5,13 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-
-
+ 
+ 
 def generate_launch_description():
 
+    ld = LaunchDescription()
+    
+    # 参数配置文件
     roboclaw_params = os.path.join(
         get_package_share_directory('osr_bringup'),
         'config',
@@ -20,8 +23,11 @@ def generate_launch_description():
         'osr_params.yaml'
     )
 
-    ld = LaunchDescription()
-    
+    # 可选参数
+    ld.add_action(DeclareLaunchArgument('enable_odometry', default_value='false'))
+    ld.add_action(DeclareLaunchArgument('joy_vel', default_value='cmd_vel'))
+
+    # 驱动板节点
     ld.add_action(
         Node(
             package='osr_control',
@@ -33,9 +39,8 @@ def generate_launch_description():
             parameters=[roboclaw_params]
         )
     )
-    ld.add_action(
-        DeclareLaunchArgument('enable_odometry', default_value='false')
-    )
+
+    # 舵机节点
     ld.add_action(
         Node(
             package='osr_control',
@@ -44,12 +49,11 @@ def generate_launch_description():
             output='screen',
             emulate_tty=True,
             respawn=True,
-            parameters=[{'centered_pulse_widths': [138, 120, 141, 120]}]  # pulse width where the corner motors are in their default position, see rover_bringup.md.
+            parameters=[{'centered_pulse_widths': [55, 116, 141, 114]}]  # pulse width where the corner motors are in their default position, see rover_bringup.md.
         )
     )
-    ld.add_action(
-        DeclareLaunchArgument('enable_odometry', default_value='false')
-    )
+    
+    # 整体控制
     ld.add_action(
         Node(
             package='osr_control',
@@ -62,37 +66,39 @@ def generate_launch_description():
                         {'enable_odometry': LaunchConfiguration('enable_odometry')}]
         )
     )
+
+    # 手柄遥控信号转控制信号节点
     ld.add_action(
         Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='teleop_twist_joy',
-            output='screen',
+            # output='screen',
             emulate_tty=True,
             respawn=True,
             parameters=[
-                # {"scale_linear.x": 0.4},  # scale to apply to drive speed, in m/s: drive_motor_rpm * 2pi / 60 * wheel radius * slowdown_factor
-                {"scale_linear.x": -0.4},  # scale to apply to drive speed, in m/s: drive_motor_rpm * 2pi / 60 * wheel radius * slowdown_factor
-                # {"axis_linear.x": 4},
-                {"axis_linear.x": 3},
-                # {"axis_angular.yaw": 0},  # which joystick axis to use for driving
-                {"axis_angular.yaw": 2},  # which joystick axis to use for driving
-                # {"scale_angular.yaw": 1.25},  # scale to apply to angular speed, in rad/s: scale_linear / min_radius(=0.45m)
-                {"axis_angular.pitch": 0},  # axis to use for in-place rotation
+                {"axis_linear.x": 1},  # 前进后退摇杆
+                {"scale_linear.x": 0.6},  # 行进速度
+                # {"scale_linear_turbo.x": 1.78},  # 速度单位，Xm/s?
+                {"axis_angular.yaw": 0},  # 转向摇杆
                 {"scale_angular.yaw": -1.25},  # scale to apply to angular speed, in rad/s: scale_linear / min_radius(=0.45m)
-                {"scale_angular.pitch": 0.25},  # scale to apply to angular speed, in rad/s: scale_linear / min_radius(=0.45m)
-                {"scale_angular_turbo.yaw": 3.95},  # scale to apply to angular speed, in rad/s: scale_linear_turbo / min_radius
-                {"scale_linear_turbo.x": 1.78},  # scale to apply to linear speed, in m/s
+                # {"scale_angular_turbo.yaw": 3.95},  # scale to apply to angular speed, in rad/s: scale_linear_turbo / min_radius
+
+                {"axis_angular.pitch": 3},  # 原地转向摇杆
+                {"scale_angular.pitch": 0.5},  # scale to apply to angular speed, in rad/s: scale_linear / min_radius(=0.45m)
+
                 # {"enable_button": 4},  # which button to press to enable movement
                 {"enable_button": 0},  # which button to press to enable movement
                 # {"enable_turbo_button": 5}  # -1 to disable turbo
-                {"enable_turbo_button": -1}  # -1 to disable turbo
+                {"enable_turbo_button": -1},  # -1 to disable turbo
             ],
             remappings=[
-                ('/cmd_vel', '/cmd_vel_intuitive')
+                ('/cmd_vel', LaunchConfiguration('joy_vel'))
             ]
         )
     )
+
+    # 手柄节点
     ld.add_action(
         Node(
             package='joy',
@@ -107,29 +113,7 @@ def generate_launch_description():
             ]        
         )
     )
-    ld.add_action(
-        Node(
-            package='osr_control',
-            executable='ina260',
-            name='ina260_node',
-            output='screen',
-            emulate_tty=True,
-            parameters=[
-                {"publish_rate": 1.0},
-                {"sensor_address": "0x45"},
-            ]        
-        )
-    )
-    # ld.add_action(
-    #     Node(
-    #         package='osr_control',
-    #         executable='joy_extras',
-    #         output='screen',
-    #         emulate_tty=True,
-    #         parameters=[
-    #             {"duty_button_index": 1}  # which button toggles duty mode on/off
-    #         ]
-    #     )
-    # )
 
+    
+    
     return ld
